9e76ba5ceddf6d6242cb5d4e414d4daac04f06f7
boyqiu-001 <1272351717@qq.com>
2024-12-24 12:37:31 +0800

    增加日志分片

--- selflogger.py
+++ selflogger.py
@@ -1,12 +1,18 @@
 import logging
+import logging.handlers
 
 def getlogger(name:str|None='my_logger',filepath:str|None='output.log'):
     # 创建 logger
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)  # 设置日志级别
-    
+    # 日志文件路径
+    log_file = 'client.log'
+    # 单个日志文件最大大小
+    max_bytes = 1024 * 1024  # 1MB
+    # 最多保留的日志文件数
+    backup_count = 20
     # 创建 file handler 并设置日志级别
-    fh = logging.FileHandler(filepath,encoding='utf-8')
+    fh = logging.handlers.RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
     fh.setLevel(logging.DEBUG)
     
     # 创建 console handler 并设置日志级别
